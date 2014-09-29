405831a68000af897ac57cca8971e1ee637e1e88
Boris Rorsvort <boris.rorsvort@gmail.com>
2014-09-29 16:15:30 +0200

    add after dom call

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 module.exports = {
   name: 'ember-cli-reading-time'
+  included: function(app) {
+    this._super.included(app);
+    app.import('bower_components/reading-time/build/readingTime.min.js');
+  }
 };
